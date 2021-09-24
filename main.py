3c24b6ac8075b7d1f7d4bd3d1ef363f3fbfe5783
CrypticSignal <hshafiq@hotmail.co.uk>
2021-09-24 20:05:03 +0100

    Create a .txt file containing the timestamp of each I-frame.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 import os
 from pathlib import Path
 import subprocess
+import sys
 
 parser = ArgumentParser()
 
@@ -16,8 +17,13 @@ parser.add_argument(
 
 args = parser.parse_args()
 filename = Path(args.file_path).name
-output_folder = f'[{filename}] I-frames'
+output_folder = filename
 os.makedirs(output_folder, exist_ok=True)
+timestamps_path = os.path.join(output_folder, "Timestamps.txt")
+
+if len(os.listdir(output_folder)) > 0:
+    print(f'I-frames-saver has already been run for {filename}')
+    sys.exit()
 
 cmd = [
     'ffmpeg', '-loglevel', 'warning', '-stats', '-i', args.file_path, '-threads', str(os.cpu_count()), 
@@ -30,15 +36,41 @@ cmd = [
 
 print('Saving the I-frames as PNG files...')
 subprocess.run(cmd)
-print('Done!')
-print('Re-naming the PNG files to adhere to the HH-MM-SS.ms format...')
+print(f'Done! {filename} has {len(os.listdir(output_folder)) - 1} I-frames.')
+
+print('Attempting to rename the PNG files to adhere to the HH-MM-SS.ms format...')
 
 for file in os.listdir(output_folder):
+    if file == "Timestamps.txt":
+        continue
+
+    if "-" in file:
+        print("It seems like the PNG files are already in the HH-MM-SS.ms format.")
+        break
+
     ms = int(Path(file).stem)
     seconds, ms = divmod(ms,1000) 
     minutes, seconds = divmod(seconds, 60) 
-    hours, minutes = divmod(minutes, 60) 
-    new_filename = f'{hours:02d}-{minutes:02d}-{seconds:02d}.{ms:03d}.png'
+    hours, minutes = divmod(minutes, 60)
+    timestamp_formatted = f'{hours:02d}-{minutes:02d}-{seconds:02d}.{ms:03d}'
+    new_filename = f'{timestamp_formatted}.png'
     os.rename(os.path.join(output_folder, file), os.path.join(output_folder, new_filename))
+
+# Create a list, where the timestamps are sorted in ascending order.
+timestamps_sorted = sorted(os.listdir(output_folder))
+# -1 because Timestamps.txt is not an I-frame.
+
+i_frame_number = 0
+with open(timestamps_path, "w"): pass
+print("Populating Timestamps.txt using the format <I-frame number> --> <timestamp>...")
+
+for timestamp in timestamps_sorted:
+    if timestamp == "Timestamps.txt":
+        continue
+
+    i_frame_number += 1
+
+    with open(timestamps_path, "a") as f:
+        f.write(f'{i_frame_number} --> {timestamp}\n')
 
 print(f'Done! Check out the "{output_folder}" folder.')
